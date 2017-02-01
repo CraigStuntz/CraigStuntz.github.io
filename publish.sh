de63e955bf2ae199ffe435f9f8387718665c3e4b
Craig Stuntz <CraigStuntz@users.noreply.github.com>
2017-01-31 22:00:47 -0500

    Add publish to publish

--- publish.sh
+++ publish.sh
@@ -2,6 +2,8 @@
 stack exec blog build
 git checkout master
 git merge develop -X theirs -m "Merge branch develop"
+git add .
+git commit -m "Publish"
 cp -a _site/. .
 rm -f posts/*.markdown
 git add .
